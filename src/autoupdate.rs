311c0cee9d2410216a4029f87ca7d8b3a20cd84f
otcova <otgercomas@gmail.com>
2022-11-18 15:11:59 +0100

    fix url autoupdate

--- src/autoupdate.rs
+++ src/autoupdate.rs
@@ -11,7 +11,7 @@ pub const VERSION: &str = env!("CARGO_PKG_VERSION");
 pub const REMOTE_VERSION_ULR: &str =
     "https://raw.githubusercontent.com/otcova/shared-minecraft-server/main/releases/last/version.txt";
 pub const REMOTE_APP_ULR: &str =
-    "https://raw.githubusercontent.com/otcova/shared-minecraft-server/main/releases/last/Shared Minecraft Server.exe";
+    "https://raw.githubusercontent.com/otcova/shared-minecraft-server/main/releases/last/Shared%20Minecraft%20Server.exe";
 
 /// If there is a new version it will download it and restart the application.
 pub fn update() {
