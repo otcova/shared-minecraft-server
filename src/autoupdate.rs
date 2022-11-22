4b505fb895f313aedf57d88c5d8cfa68a3d4077e
otcova <otgercomas@gmail.com>
2022-11-22 19:11:21 +0100

    auto update: delete old executable after exit

--- src/autoupdate.rs
+++ src/autoupdate.rs
@@ -1,7 +1,7 @@
 use crate::{fetch::*, process::run_detached_process};
 use std::{
     env::current_exe,
-    fs::{rename, write},
+    fs::{remove_file, rename, write},
     io,
     process::exit,
 };
@@ -15,6 +15,8 @@ pub const REMOTE_APP_ULR: &str =
 
 /// If there is a new version it will download it and restart the application.
 pub fn update() {
+    let _ = remove_old_version();
+
     if let Some(remote_version) = fetch_str(REMOTE_VERSION_ULR) {
         if remote_version.as_str() > VERSION {
             let _ = install_new_version();
@@ -22,13 +24,24 @@ pub fn update() {
     }
 }
 
+fn remove_old_version() -> io::Result<()> {
+    let mut old_exe_path = current_exe()?;
+    old_exe_path.set_file_name(format!(".shared-minecraft-server.old"));
+    remove_file(&old_exe_path).unwrap();
+    Ok(())
+}
+
 fn install_new_version() -> io::Result<()> {
     if let Some(new_app_data) = fetch_bin(REMOTE_APP_ULR) {
         let exe_path = current_exe()?;
         let mut temp_exe_path = exe_path.clone();
+        let mut old_exe_path = exe_path.clone();
+
         temp_exe_path.set_file_name(format!(".{}.tmp", env!("CARGO_PKG_NAME")));
+        old_exe_path.set_file_name(format!(".shared-minecraft-server.old"));
 
         write(&temp_exe_path, new_app_data)?;
+        rename(&exe_path, &old_exe_path)?;
         rename(&temp_exe_path, &exe_path)?;
 
         run_detached_process(&exe_path)?;
